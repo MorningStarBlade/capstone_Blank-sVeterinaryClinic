6795ea837648b8f78e93de3552850a6117eeceb0
Anya Dinger.pub <adinger@student.cvtc.edu>
2023-11-09 08:10:01 -0600

    Created a basic Express application

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5163;
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true}))
+  .get("/", async(req, res)=> {
+      res.send("Hello From Blank's Veterinary Clinic");
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
