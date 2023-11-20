479ad9485f744871df0daac3be4b30a00002217b
Tawna Shampo <tawnashampo@gmail.com>
2023-11-20 13:39:23 -0600

    added express session to keep user logged in across pages and a logout feature

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5163;
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -15,119 +16,55 @@ const pool = new Pool({
   }
 });
 
+app.use(cookieParser());
+app.use(sessions({
+  secret: "capstone",
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+  resave: false
+}));
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Middleware to add user to res.locals for all views
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render('index');
 });
 
-app.get("/index.ejs",(req, res) => {
+app.get("/index.ejs", (req, res) => {
   res.render('index');
 });
 
-app.get("/about.ejs",(req, res) => {
+app.get("/about.ejs", (req, res) => {
   res.render('about');
 });
 
-app.get("/WellnessWizard.ejs",(req, res) => {
+app.get("/WellnessWizard.ejs", (req, res) => {
   res.render('WellnessWizard');
 });
 
-app.get("/resources.ejs",(req, res) => {
+app.get("/resources.ejs", (req, res) => {
   res.render('resources');
 });
 
-app.get("/login.ejs",(req, res) => {
+app.get("/login.ejs", (req, res) => {
   res.render('login');
 });
 
-app.get("/createAccount.ejs",(req, res) => {
+app.get("/createAccount.ejs", (req, res) => {
   res.render('createAccount');
 });
 
-// For Wellness Wizard
-
-app.get("/app", (req, res) => {
-  res.redirect('/WellnessWizard');
-});
-
-app.post("/app", async(req, res) => {
-  res.set({
-    "Content-Type": "application/json"
-  });
-
-  try {
-    const client = await pool.connect();
-
-    const name = req.body.name;
-    const age = req.body.age;
-    const animal = req.body.animal;
-    const breed = req.body.breed;
-    const reason = req.body.reason;
-    const date = req.body.date;
-    const notes = req.body.notes;
-
-    const insertSql = `INSERT INTO appointments (name, age, animal, breed, reason, date, notes) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-
-    const insert = await client.query(insertSql, [name, age, animal, breed, reason, date, notes]);
-
-    const response = true;
-
-    res.json(response);
-
-    client.release();
-
-  } catch (err){
-    console.error(err);
-    res.json({
-      error: err
-    });
-  }
-});
-
-// For create account
-app.post("/createAccount", async (req, res) => {
-  try {
-    const client = await pool.connect();
-
-    const firstName = req.body.fname;
-    const lastName = req.body.lname;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    // Hash the password using bcrypt
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const insertSql = `
-      INSERT INTO users (first_name, last_name, email, password)
-      VALUES ($1, $2, $3, $4)
-    `;
-
-    await client.query(insertSql, [firstName, lastName, email, hashedPassword]);
-
-    // Redirect to the login page with a success message
-    const successMessage = `You've successfully created an account now you can log in!`;
-
-    res.render('login', { message: successMessage });
-
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      error: err.message || "Internal Server Error",
-    });
-  }
-});
-
-
-
-
-// For Login
 app.post("/login", async (req, res) => {
   try {
     const client = await pool.connect();
@@ -147,15 +84,20 @@ app.post("/login", async (req, res) => {
       const passwordMatch = await bcrypt.compare(enteredPassword, storedPassword);
 
       if (passwordMatch) {
+        // Store user information in the session
+        req.session.user = {
+          id: result.rows[0].user_id,
+          firstName: result.rows[0].first_name,
+          // Add other user-related information as needed
+        };
+
         const welcomeMessage = `Welcome, ${result.rows[0].first_name}!`;
 
         res.render('index', { message: welcomeMessage });
       } else {
-        // Render the login page with an error message
         res.render('login', { errorMessage: "Invalid email or password" });
       }
     } else {
-      // Render the login page with an error message
       res.render('login', { errorMessage: "Invalid email or password" });
     }
 
@@ -168,8 +110,24 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    } else {
+      res.redirect('/login.ejs');
+    }
+  });
+});
 
-
-
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    } else {
+      res.redirect('/login.ejs');
+    }
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
